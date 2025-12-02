c59cfd411694bb4601f56f213c5760b1715082ae
Luke Michaels <lmichaels@gmail.com>
2025-12-01 22:19:52 -0800

    add scroll to top between pages
    can’t believe i almost forgot this :D

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
+import ScrollToTop from "./components/ScrollToTop";
 import GalaxyBackground from "./components/GalaxyBackground.jsx";
 import Hero from "./components/Hero.jsx";
 import Footer from "./components/Footer.jsx";
@@ -11,6 +12,7 @@ export default function App() {
   return (
     <div className="app has-galaxy">
       <GalaxyBackground />
+      <ScrollToTop />
       <Header />
       <Hero />
       <main className="site-main" id="main-content" tabIndex="-1">
