58f01f456893f77d886f02056806770ef26b7996
Luke Michaels <lmichaels@gmail.com>
2025-12-01 23:14:52 -0800

    more robust scroll to top

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -3,15 +3,15 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const location = useLocation();
 
   useEffect(() => {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: "smooth"
+      behavior: "smooth",
     });
-  }, [pathname]);
+  }, [location.key]);
 
   return null;
 }
