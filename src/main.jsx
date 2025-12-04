671f1c234d1aa3c95ef282e6549f597d5701e53c
Luke Michaels <lmichaels@gmail.com>
2025-12-03 23:23:43 -0800

    fix fuplicate tag

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/globals.scss";
-import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
